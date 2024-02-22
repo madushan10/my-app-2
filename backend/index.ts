29dddc841a018f6f1d2ef40d3a280927b641caa5
Madushan <Madushan@DESKTOP-O2MVSJ2>
2024-02-22 16:50:21 +0530

    13

--- backend/index.ts
+++ backend/index.ts
@@ -40,7 +40,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
-
+  res.sendStatus(200);
   if (req.method === 'OPTIONS') {
     res.sendStatus(200);
   } else {
