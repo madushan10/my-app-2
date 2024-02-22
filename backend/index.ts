c45bc6dcd1ba6c598a68c1d0db145c65699ff20a
Madushan <Madushan@DESKTOP-O2MVSJ2>
2024-02-22 16:42:13 +0530

    11

--- backend/index.ts
+++ backend/index.ts
@@ -36,7 +36,7 @@ app.get("/", (req: Request, res: Response) => {
 
 app.use("/api/chat", chatRouter);
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://llama-frontend-rho.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
